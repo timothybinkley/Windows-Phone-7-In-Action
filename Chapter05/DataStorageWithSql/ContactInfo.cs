cc2e6b7f3118bce27838ddbb972d108182a0b2b6
Tim Binkley-Jones <timothybinkley@gmail.com>
2011-04-30 11:01:39 -0600

    Copied iso storage project for sql storage project

---
+++ Chapter05/DataStorageWithSql/ContactInfo.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace DataStorage
+{
+    public class ContactInfo : INotifyPropertyChanged
+    {
+        private string name;
+        public String Name
+        {
+            get { return name; }
+            set
+            {
+                if (value != name)
+                {
+                    name = value;
+                    NotifyPropertyChanged("Name");
+                }
+            }
+        }
+
+        public string surname;
+        public String Surname
+        {
+            get { return surname; }
+            set
+            {
+                if (value != surname)
+                {
+                    surname = value;
+                    NotifyPropertyChanged("Surname");
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged(String propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (null != handler)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
