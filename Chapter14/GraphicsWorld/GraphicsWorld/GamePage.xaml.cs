f07f7e31700bc0427dd05f6343c62e9cefca324d
Tim Binkley-Jones <timothybinkley@gmail.com>
2011-05-24 22:22:25 -0600

    Added real WP7.1 sample project for chapter 14 and removed temporary placeholder.

---
+++ Chapter14/GraphicsWorld/GraphicsWorld/GamePage.xaml.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace GraphicsWorld
+{
+    public partial class GamePage : PhoneApplicationPage
+    {
+        GameTimer timer;
+        ContentManager content;
+        SpriteBatch spriteBatch;
+        GamePlayComponent gamePlay = new GamePlayComponent();
+        IGamePlayInput input;
+        int frameCountAfterMotionStopped;
+
+        public GamePage()
+        {
+            InitializeComponent();
+
+            // Get the application's ContentManager
+            content = (Application.Current as App).Content;
+
+            // Create a timer for this page
+            timer = new GameTimer();
+            timer.UpdateInterval = TimeSpan.FromTicks(333333);
+            timer.Update += OnUpdate;
+            timer.Draw += OnDraw;
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+
+            // Set the sharing mode of the graphics device to turn on XNA rendering
+            SharedGraphicsDeviceManager.Current.GraphicsDevice.SetSharingMode(true);
+
+            spriteBatch = new SpriteBatch(SharedGraphicsDeviceManager.Current.GraphicsDevice);
+
+            input = new DemoInput();
+            gamePlay.Initialize(content, input);
+
+            // Start the timer
+            timer.Start();
+
+            base.OnNavigatedTo(e);
+        }
+
+        protected override void OnNavigatedFrom(NavigationEventArgs e)
+        {
+            // Stop the timer
+            timer.Stop();
+
+            // Set the sharing mode of the graphics device to turn off XNA rendering
+            SharedGraphicsDeviceManager.Current.GraphicsDevice.SetSharingMode(false);
+
+            base.OnNavigatedFrom(e);
+        }
+
+        /// <summary>
+        /// Allows the page to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        private void OnUpdate(object sender, GameTimerEventArgs e)
+        {
+            input.Update();
+            gamePlay.Update();
+
+            if (!input.MoveForward && !input.MoveBackward
+                && !input.TurnLeft && !input.TurnRight)
+            {
+                if (frameCountAfterMotionStopped > 2)
+                {
+                    GameTimer.SuppressFrame();
+                }
+            }
+            else
+            {
+                frameCountAfterMotionStopped = 0;
+            }
+
+            if (!gamePlay.IsPlaying && NavigationService.CanGoBack)
+                NavigationService.GoBack();
+        }
+
+        /// <summary>
+        /// Allows the page to draw itself.
+        /// </summary>
+        private void OnDraw(object sender, GameTimerEventArgs e)
+        {
+            var device = SharedGraphicsDeviceManager.Current.GraphicsDevice;
+            device.Clear(Color.CornflowerBlue);
+            device.DepthStencilState = DepthStencilState.Default;
+            device.RasterizerState = RasterizerState.CullCounterClockwise;
+            gamePlay.Draw();
+        }
+
+    }
+}
---
+++ Chapter14/GraphicsWorld/GraphicsWorld/IGamePlayInput.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Xna.Framework.Content;
+
+namespace GraphicsWorld
+{
+    public interface IGamePlayInput
+    {
+        bool MoveForward { get; }
+        bool MoveBackward { get; }
+        bool TurnLeft { get; }
+        bool TurnRight { get; }
+
+        void Initialize(ContentManager content);
+        void Start();
+        void Stop();
+        void Update();
+        void Draw();
+    }
+
+}
