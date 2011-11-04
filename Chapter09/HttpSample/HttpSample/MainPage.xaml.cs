b39779100651a6f1099269ebcbd7bf8a5a55f65d
Michael Sync <mchlsync@gmail.com>
2011-11-05 00:39:08 +0800

    add some more examples for ch9

---
+++ Chapter09/HttpSample/HttpSample/MainPage.xaml.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+using System.IO;
+using System.Xml.Linq;
+using System.Diagnostics;
+using System.Xml;
+
+namespace HttpSample {
+    public partial class MainPage : PhoneApplicationPage {
+        // Constructor
+        public MainPage() {
+            InitializeComponent();
+            //http://www.odata.org/developers/odata-sdk#http://services.odata.org/(S(readwrite))/OData/OData.svc/Suppliers
+            //http://www.odata.org/developers/protocols/uri-conventions#QueryStringOptions
+            //different HTTP verbs (GET for reading, PUT for creating, POST for updating, and DELETE for deleting).
+
+          
+
+
+            Dispatcher.
+
+            //request.BeginGetResponse(asynchronousResult => {
+            //    HttpWebRequest request2 = (HttpWebRequest)asynchronousResult.AsyncState;
+            //    HttpWebResponse response = (HttpWebResponse)request2.EndGetResponse(asynchronousResult);
+            //    if (((HttpWebResponse)response).StatusCode == HttpStatusCode.Created /* 201 */ ) {
+            //        Console.WriteLine(
+            //          "Created category @ Location= {0}", response.Headers["Location"]);
+            //    }
+            //    else {
+            //        Console.WriteLine("Category creation failed");
+            //    }
+            //}, request);
+
+        }
+        static void CreateCategory(Action complete) {           
+            XNamespace ds = "http://schemas.microsoft.com/ado/2007/08/dataservices";
+            XNamespace dsmd = "http://schemas.microsoft.com/ado/2007/08/dataservices/metadata";
+            var content =
+              new XElement(dsmd + "properties",
+                new XElement(ds + "CategoryName", "Dessert Topping"),
+                new XElement(ds + "Description", "Toppings for desserts")
+              );
+
+            
+            XNamespace atom = "http://www.w3.org/2005/Atom";
+            var entry =
+              new XElement(atom + "entry",
+                new XElement(atom + "title", "A new Northwind category"),
+                new XElement(atom + "id", string.Format("urn:uuid:{0}", Guid.NewGuid())),
+                new XElement(atom + "updated", DateTime.Now),
+                new XElement(atom + "author",
+                new XElement(atom + "name", "Chris Sells")),
+                new XElement(atom + "content",
+                  new XAttribute("type", "application/xml"),
+                  content)
+              );
+
+            // Send the HTTP request
+            HttpWebRequest request =
+             (HttpWebRequest)HttpWebRequest.Create(@"http://localhost:32026/Northwind/Northwind.svc/Categories/");
+            request.Method = "POST";
+            request.Accept = "application/atom+xml";
+            request.ContentType = "application/atom+xml;type=entry";
+            request.BeginGetRequestStream(getRequestResult => {
+                HttpWebRequest request1 = (HttpWebRequest)getRequestResult.AsyncState;
+                var stream = request1.EndGetRequestStream(getRequestResult);
+                using (var writer = XmlWriter.Create(stream)) {
+                    entry.WriteTo(writer);        
+                }
+                complete.Invoke();
+            }, request);
+        }
+
+        private static void ReadCallback(IAsyncResult asynchronousResult) {
+            try {
+                HttpWebRequest request = (HttpWebRequest)asynchronousResult.AsyncState;
+                HttpWebResponse response = (HttpWebResponse)request.EndGetResponse(asynchronousResult);
+
+                XNamespace atom = "http://www.w3.org/2005/Atom";
+
+                var xdoc = XDocument.Load(response.GetResponseStream());
+                foreach (var entity in xdoc.Descendants(atom + "entry")) {
+                    var id = entity.Elements(atom + "id");
+                    var title = entity.Elements(atom + "title");
+                }
+            }
+            catch (Exception ex) {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+    }
+}
---
+++ Chapter09/HttpSample/odatasampleservices/services.odata.org/ODataWeb/Utils.cs
@@ -0,0 +1,40 @@
+﻿//*********************************************************
+//
+//    Copyright (c) Microsoft. All rights reserved.
+//    This code is licensed under the Microsoft Public License.
+//    THIS CODE IS PROVIDED *AS IS* WITHOUT WARRANTY OF
+//    ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING ANY
+//    IMPLIED WARRANTIES OF FITNESS FOR A PARTICULAR
+//    PURPOSE, MERCHANTABILITY, OR NON-INFRINGEMENT.
+//
+//*********************************************************
+
+namespace ODataWeb
+{
+    using System;
+    using System.Data.Services;
+    using System.IO;
+    using System.Web;
+
+    /// <summary>
+    /// Utility Functions for ODataWeb
+    /// </summary>
+    public class Utils
+    {
+        /// <summary>
+        /// Resolve the physical path of files related to the application root
+        /// </summary>
+        /// <param name="relativePath"></param>
+        /// <returns></returns>
+        public static string ResolvePhysicalPath(string relativePath)
+        {
+            if (HttpContext.Current == null)
+            {
+                throw new DataServiceException(500, "Cannot create database. Unable to resolve physical path to the script.");
+            }
+
+            string appRoot = HttpContext.Current.Request.PhysicalApplicationPath;
+            return Path.Combine(appRoot, relativePath);
+        }
+    }
+}
