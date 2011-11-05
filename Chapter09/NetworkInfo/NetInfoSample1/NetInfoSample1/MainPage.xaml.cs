b9b79fdc560d04fe0a0d9cc968aa48a5e1674a37
Michael Sync <mchlsync@gmail.com>
2011-11-05 15:59:22 +0800

    added new API (DeviceNetwork Information) in NetInfoSample

--- Chapter09/NetworkInfo/NetInfoSample1/NetInfoSample1/MainPage.xaml.cs
+++ Chapter09/NetworkInfo/NetInfoSample1/NetInfoSample1/MainPage.xaml.cs
@@ -10,69 +10,74 @@ using System.Windows.Media;
 using System.Windows.Media.Animation;
 using System.Windows.Shapes;
 using Microsoft.Phone.Controls;
-using System.Net.NetworkInformation;
-using Phone = Microsoft.Phone.Net.NetworkInformation;
+using Microsoft.Phone.Net.NetworkInformation;
 
-namespace NetInfoSample1
-{
-    public partial class MainPage : PhoneApplicationPage
-    {
+namespace NetInfoSample1 {
+    public partial class MainPage : PhoneApplicationPage {
         // Constructor
-        public MainPage()
-        {
+        public MainPage() {
             InitializeComponent();
 
-            ShowNetwork();
+            UsingDeviceInformationAPI();
 
-            NetworkChange.NetworkAddressChanged += new NetworkAddressChangedEventHandler(NetworkChange_NetworkAddressChanged);
+            //NetworkInterface.GetIsNetworkAvailable();
         }
 
-        void NetworkChange_NetworkAddressChanged(object sender, EventArgs e)
-        {
-            ShowNetwork();
+        void UsingDeviceInformationAPI() {
+            CellularMobileOperatorTextBlock.Text = string.Format("CellularMobileOperator : {0}", DeviceNetworkInformation.CellularMobileOperator);
+            IsCellularDataEnabledTextBlock.Text = string.Format("IsCellularDataEnabled : {0}", DeviceNetworkInformation.IsCellularDataEnabled);
+            IsCellularDataRoamingEnabledTextBlock.Text = string.Format("IsCellularDataRoamingEnabled : {0}", DeviceNetworkInformation.IsCellularDataRoamingEnabled);
+            IsNetworkAvailableTextBlock.Text = string.Format("IsNetworkAvailable : {0}", DeviceNetworkInformation.IsNetworkAvailable);
+            IsWiFiEnabledTextBlock.Text = string.Format("IsWiFiEnabled : {0}", DeviceNetworkInformation.IsWiFiEnabled);
+
+            //DeviceNetworkInformation.ResolveHostNameAsync(new DnsEndPoint("http://google.com", 0), (b) => {
+            //    Console.Write(b.HostName);
+            //}, null);
+
+            DeviceNetworkInformation.NetworkAvailabilityChanged += new EventHandler<NetworkNotificationEventArgs>(DeviceNetworkInformation_NetworkAvailabilityChanged);
         }
-
-        private void ShowNetwork()
-        {
-            try
-            {
-                var netInterfaceType = Phone.NetworkInterface.NetworkInterfaceType;
-
-                networkTypeTextblock.Text = string.Format("Network Status : {0}", netInterfaceType);
-
-                if (netInterfaceType == Phone.NetworkInterfaceType.None)
-                {
-                    netIndicatorEllipse.Fill = new SolidColorBrush(Colors.Red);
-                    testConnectionTextblock.IsEnabled = false;
-                }
-
-                networkStatusTextblock.Text = string.Format("Network Status : {0}", Phone.NetworkInterface.GetIsNetworkAvailable() ? "Online" : "Off");
+        void DeviceNetworkInformation_NetworkAvailabilityChanged(object sender, NetworkNotificationEventArgs e) {
+            string change = string.Empty;
+            switch (e.NotificationType) {
+                case NetworkNotificationType.InterfaceConnected:
+                    change = "Connected to ";
+                    break;
+                case NetworkNotificationType.InterfaceDisconnected:
+                    change = "Disconnected from ";
+                    break;
+                case NetworkNotificationType.CharacteristicUpdate:
+                    change = "Characteristics changed for ";
+                    break;
+                default:
+                    change = "Unknown change with ";
+                    break;
             }
-            catch (Exception ex)
-            {
-                networkStatusTextblock.Text = string.Format("{0} using NetworkInterface:\r\n{1}", ex.GetType().Name, ex.Message);
-            }
+
+            string changeInformation = String.Format(" {0} {1} {2} ({3})",
+                  DateTime.Now.ToString(), change, e.NetworkInterface.InterfaceName,
+                  e.NetworkInterface.InterfaceType.ToString());
+            
+            Dispatcher.BeginInvoke(() => {
+                NetworkAvailabilityChangedTextBlock.Text += changeInformation;
+            });  
         }
 
+        
+        //private void ShowNetwork() {
+        //    try {
+        //        var netInterfaceType = Phone.NetworkInterface.NetworkInterfaceType;
 
+        //        networkTypeTextblock.Text = string.Format("Network Status : {0}", netInterfaceType);
 
-        void client_DownloadStringCompleted(object sender, DownloadStringCompletedEventArgs e)
-        {
-            if (e.Error == null)
-            {
-                MessageBox.Show("Yah!! Got it!");
-            }
-            else
-            {
-                MessageBox.Show(e.Error.Message);
-            }
-        }
+        //        if (netInterfaceType == Phone.NetworkInterfaceType.None) {
+        //            netIndicatorEllipse.Fill = new SolidColorBrush(Colors.Red);                   
+        //        }
 
-        private void testConnectionTextblock_Click(object sender, RoutedEventArgs e)
-        {
-            var client = new WebClient();
-            client.DownloadStringCompleted += new DownloadStringCompletedEventHandler(client_DownloadStringCompleted);
-            client.DownloadStringAsync(new Uri("http://microsoft.com"));
-        }
+        //        networkStatusTextblock.Text = string.Format("Network Status : {0}", Phone.NetworkInterface.GetIsNetworkAvailable() ? "Online" : "Off");
+        //    }
+        //    catch (Exception ex) {
+        //        networkStatusTextblock.Text = string.Format("{0} using NetworkInterface:\r\n{1}", ex.GetType().Name, ex.Message);
+        //    }
+        //}
     }
 }
