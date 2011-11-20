717bc36ab9f150353dd589bbbba55470a40dff02
Michael Sync <mchlsync@gmail.com>
2011-11-21 04:30:13 +0800

    added httpwebrequest version to webclient sample

--- Chapter09/HttpSample/HttpSample/MainPage.xaml.cs
+++ Chapter09/HttpSample/HttpSample/MainPage.xaml.cs
@@ -19,17 +19,42 @@ namespace HttpSample {
             InitializeComponent();
         }
 
+        //private void getImageButton_Click(object sender, RoutedEventArgs e) {
+        //    HttpWebRequest webrequest = (HttpWebRequest)WebRequest
+        //        .Create(new Uri("http://manning.com/perga/perga_cover150.jpg"));
+        //    webrequest.Method = "GET";
+        //    webrequest.BeginGetResponse(
+        //        new AsyncCallback(ReadCallback), webrequest);
+        //}
+
+        //private void ReadCallback(IAsyncResult result) {
+        //    HttpWebRequest request = (HttpWebRequest)result.AsyncState;
+        //    HttpWebResponse response = (HttpWebResponse)request.EndGetResponse(result);
+
+        //    Stream stream = response.GetResponseStream();
+        //    Dispatcher.BeginInvoke(() => {
+        //        var bitmap = new BitmapImage();
+        //        bitmap.SetSource(stream);
+        //        imageControl.Source = bitmap;
+        //    });
+        //}
+
+
+
+
         private void getImageButton_Click(object sender, RoutedEventArgs e) {
-            var client = new WebClient(); 
-            client.OpenReadCompleted += new OpenReadCompletedEventHandler(client_OpenReadCompleted); 
-            client.OpenReadAsync(new Uri("http://manning.com/perga/perga_cover150.jpg")); 
+            var client = new WebClient();
+            client.OpenReadCompleted += new
+                OpenReadCompletedEventHandler(client_OpenReadCompleted);
+            client.OpenReadAsync(
+                new Uri("http://manning.com/perga/perga_cover150.jpg"));
         }
 
         void client_OpenReadCompleted(object sender, OpenReadCompletedEventArgs e) {
-            if (e.Error == null) { 
-                Stream stream = e.Result; 
+            if (e.Error == null) {
+                Stream stream = e.Result;
                 var bitmap = new BitmapImage();
-                bitmap.SetSource(stream);  
+                bitmap.SetSource(stream);
                 imageControl.Source = bitmap;
             }
             else {
